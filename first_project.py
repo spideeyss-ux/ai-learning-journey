bebfae5b06d05300f261b9b97e9385bf37f20268
spideeyss-ux <spideeyss@gmail.com>
2026-04-23 02:44:16 +0530

    Update first_project.py

--- first_project.py
+++ first_project.py
@@ -1,1 +1,15 @@
-5
+print("🤖 Welcome to AI Mini Chat")
+
+name = input("Enter your name: ")
+print("Hello", name)
+
+feeling = input("How are you feeling today? ")
+
+if feeling.lower() == "happy":
+    print("That's great! Keep going 💪")
+elif feeling.lower() == "sad":
+    print("Don't worry, better days are coming ❤️")
+else:
+    print("Nice! Stay focused and keep improving 🚀")
+
+print("Thanks for using this program 😎")
